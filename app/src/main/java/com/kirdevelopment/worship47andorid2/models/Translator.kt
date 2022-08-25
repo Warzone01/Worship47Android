05f66391cd27cbb5292410b8d6bbdc53044fbebd
kirill <kirill_gulvaniuk>
2022-08-25 16:49:26 +0300

    исправлен функционал категорий

--- app/src/main/java/com/kirdevelopment/worship47andorid2/models/Translator.kt
+++ app/src/main/java/com/kirdevelopment/worship47andorid2/models/Translator.kt
@@ -1,9 +1,9 @@
 package com.kirdevelopment.worship47andorid2.models
 
 data class Translator(
-    val count: Int,
-    val id: Int,
-    val name: String,
-    val `protected`: Boolean,
-    val slug: String
+    val count: Int = 0,
+    val id: Int = 0,
+    val name: String = "",
+    val `protected`: Boolean = false,
+    val slug: String = ""
 )
